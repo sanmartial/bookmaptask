ad21292e00184d192e6cb1211ff86bd8d3e9ea7a
Roman Globa <globaroman@gmail.com>
2024-04-08 23:55:45 +0300

    Fixed code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,30 +2,27 @@ import service.DataService;
 import service.InputOutputData;
 import service.impl.DataServiceImpl;
 import service.impl.InputOutputDataImpl;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
-
     private static final String PATH_TO_FILE_READ = "input.txt";
     private static final String PATH_TO_FILE_WRITE = "output.txt";
 
-
-
-
     public static void main(String[] args) {
-
         final InputOutputData inputOutputData = new InputOutputDataImpl();
         final DataService dataService = new DataServiceImpl();
+
         List<String> inputDataFromFiles = inputOutputData.inputDataFromFile(PATH_TO_FILE_READ);
-        List<String> outcomes = new ArrayList<>();
-        for (int i = 2; i < inputDataFromFiles.size(); i++) {
-            outcomes.add(String.valueOf(dataService.processingData(inputDataFromFiles.get(1), inputDataFromFiles.get(i))));
-        }
+        String baseLine = inputDataFromFiles.get(1);
+
+        List<Integer> outcomes = inputDataFromFiles.stream()
+                .skip(2)
+                .parallel()
+                .map(data -> dataService.processingData(baseLine, data))
+                .collect(Collectors.toList());
 
         inputOutputData.outputDataToFile(PATH_TO_FILE_WRITE, outcomes);
-
-
     }
 }
--- src/main/java/service/impl/InputOutputDataImpl.java
+++ src/main/java/service/impl/InputOutputDataImpl.java
@@ -2,45 +2,34 @@ package service.impl;
 
 
 import service.InputOutputData;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputOutputDataImpl implements InputOutputData {
 
-
     @Override
     public List<String> inputDataFromFile(String path) {
-        List<String> list = new ArrayList<>();
-        try(BufferedReader br = new BufferedReader(new FileReader(path))){
-            String line;
-            while ((line = br.readLine()) != null){
-                list.add(line);
-            }
+        try {
+            return Files.readAllLines(Paths.get(path));
+        } catch (IOException e) {
+            throw new RuntimeException("Error! Can not read data from file " + path, e);
         }
-        catch (IOException e){
-            throw new RuntimeException("Error! Can not read data from file " + path);
-        }
-
-   return list;
-
     }
 
     @Override
-    public void outputDataToFile(String path, List<String> outcomes) {
-
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
-            for (String line : outcomes) {
-                bw.write(line);
-                bw.write("\n");
-            }
+    public void outputDataToFile(String path, List<Integer> outcomes) {
+        try {
+            Files.write(Paths.get(path), outcomes.stream().map(Object::toString).collect(Collectors.toList()));
         } catch (IOException e) {
-            throw new RuntimeException("Error! Can not write data to file " + path);
+            throw new RuntimeException("Error! Can not write data to file " + path, e);
         }
     }
 }
